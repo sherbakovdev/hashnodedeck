605c0b061c78b19e19e86585f7ec79eff9ab3757
Nikita Sherbakov <nikita.lpr@gmail.com>
2021-01-22 15:46:40 +0200

    Add Google Analytics

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script async src='https://www.googletagmanager.com/gtag/js?id=G-D5Q9X597E4'></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-D5Q9X597E4');
+             `,
+            }}
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
